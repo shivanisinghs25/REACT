fd2ffd6caf82ef813b565a1016a4c99eec0c934c
shivanisinghs25 <shivanisinghs003@gmail.com>
2024-02-12 23:18:48 +0530

    episode-01

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 
-{/* <div id="parent">
-    <div id="child1">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div> */}
-
 const parent = React.createElement("div", {id: "parent"}, 
 [React.createElement("div", {id: "child1"},
 [React.createElement("h1", {}, "I'm h1 tag"),
